9f56f2679ff98d0c5ce6ecd51ae2a8e040487868
Neo Ighodaro <neo@creativitykills.co>
2019-08-14 11:05:01 +0200

    Updated the landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,13 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>{{ config('app.name') }}</title>
-
-        <!-- Fonts -->
+        <title>Cmpsr – Generate Composer Packages Using HTTP</title>
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
         <style>
             html, body {
                 background-color: #fff;
